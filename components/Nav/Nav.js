dd7998465b516c65caf8c08a3344c0e20f994361
Nainik Mehta <nainik.m@crestinfosystemns.net>
2022-07-08 12:27:02 +0530

    Next JS learning

---
+++ components/Nav/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div>
+      <nav className="navbar bg-light">
+        <div className="container-fluid">
+          <span className="navbar-brand mb-0 h1">Navbar</span>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Nav;
